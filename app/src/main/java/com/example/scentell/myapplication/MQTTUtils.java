fd91e89d6902824f409b69be29df173ec7663750
Sylvain Centelles <sylvain.centelles@intel.com>
2017-01-27 14:12:54 +0100

    Remove static from MQTTUtils, and manage onPause and onResume states and associated MQTT publish. Also check for connection status befor publishing...

--- app/src/main/java/com/example/scentell/myapplication/MQTTUtils.java
+++ app/src/main/java/com/example/scentell/myapplication/MQTTUtils.java
@@ -5,26 +5,42 @@ package com.example.scentell.myapplication;
  */
 
 
+import android.util.Log;
+
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import static android.content.ContentValues.TAG;
 
 public class MQTTUtils {
 
-    private static MqttClient client;
+    private MqttClient client;
 
-    public static MqttClient getClient() {
+    public MqttClient getClient() {
         return client;
     }
 
-    public static boolean connect(String url) {
+
+    private String _clientName;
+    private String _url;
+    private int _port;
+
+    public MQTTUtils(String clientName, String url, int port){
+        _clientName = clientName;
+        _url = url;
+        _port = port;
+
+    }
+
+    public boolean connect() {
         try {
             MemoryPersistence persistance = new MemoryPersistence();
-            client = new MqttClient("tcp://" + url + ":1357", "client1", persistance);
+            client = new MqttClient("tcp://" + _url + ":" + _port, _clientName, persistance);
             client.connect();
+
             return true;
         } catch (MqttException e) {
             e.printStackTrace();
@@ -34,8 +50,27 @@ public class MQTTUtils {
         return false;
     }
 
-    public static boolean pub(String topic, String payload) {
+    public void disconnect()
+    {
+        if(!client.isConnected()) {
+            try {
+            client.disconnect();
+            } catch (MqttPersistenceException e) {
+                e.printStackTrace();
+            }
+             catch (MqttException e) {
+                e.printStackTrace();
+             }
+        }
+    }
+
+    public boolean pub(String topic, String payload) {
         MqttMessage message = new MqttMessage(payload.getBytes());
+        while(!client.isConnected()){
+            Log.v(TAG, "Trying to reconnect");
+            connect();
+
+        }
         try {
             client.publish(topic, message);
             return true;
--- app/src/main/java/com/example/scentell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/scentell/myapplication/MainActivity.java
@@ -29,19 +29,19 @@ class SettingsContentObserver extends ContentObserver {
 
     int previousVolume;
     Context context;
-
+    private MQTTUtils mqttClient;
 
     public SettingsContentObserver(Handler handler) {
         super(handler);
     }
 
-    public SettingsContentObserver(Context c, Handler handler) {
+    public SettingsContentObserver(Context c, Handler handler, MQTTUtils mqttClient) {
         super(handler);
         context=c;
 
         AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         previousVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
-      //  Log.v(TAG, "init volume : ");
+      //  Log.v(TAG, "init volume : ");mqttClient
 
 
     }
@@ -66,13 +66,13 @@ class SettingsContentObserver extends ContentObserver {
         {
             Log.v(TAG, "Decreased");
             previousVolume=currentVolume;
-            MQTTUtils.pub("ECS/state", "2");
+            //myMqtt.pub("ECS/state", "2");
         }
         else if(delta<0)
         {
             Log.v(TAG, "Increased");
             previousVolume=currentVolume;
-            MQTTUtils.pub("ECS/state", "1");
+            //myMqtt.pub("ECS/state", "1");
         }
 
 
@@ -108,6 +108,11 @@ public class MainActivity extends AppCompatActivity {
 
     SettingsContentObserver mSettingsContentObserver ;
 
+    String server = "77.146.227.62";
+    String clientName = "IPTV_APP";
+    int port = 1357;
+    MQTTUtils myMqtt = new MQTTUtils(clientName, server, port);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -118,18 +123,18 @@ public class MainActivity extends AppCompatActivity {
         MediaButtonReceiver r = new MediaButtonReceiver();
         registerReceiver(r, filter);
 
-        mSettingsContentObserver = new SettingsContentObserver(this, new Handler());
+        mSettingsContentObserver = new SettingsContentObserver(this, new Handler(), myMqtt);
         getApplicationContext().getContentResolver().registerContentObserver(android.provider.Settings.System.CONTENT_URI, true, mSettingsContentObserver );
 
 
 
 
-        String server = "77.146.227.62";
-        boolean result =  MQTTUtils.connect(server);
+        boolean result =  myMqtt.connect();
+
         if(result) {
 
             myTextView.setText("Connection established");
-//            MQTTUtils.pub("ECS/state", "1");
+//            myMqtt.pub("ECS/state", "1");
 
 
         }
@@ -142,17 +147,37 @@ public class MainActivity extends AppCompatActivity {
     public void onPause()
     {
         Log.v(TAG, "Pause detected");
-        MQTTUtils.pub("ECS/state", "4");
+        myMqtt.pub("ECS/state", "4");
         super.onPause();
     }
     @Override
+    public void onResume()
+    {
+        Log.v(TAG, "Pause detected");
+        myMqtt.pub("ECS/state", "5");
+        super.onResume();
+    }
+    @Override
+    public void onStop()
+    {
+        Log.v(TAG, "Stop detected");
+        myMqtt.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode==KeyEvent.KEYCODE_HOME){
-            //Send your own custom broadcast message
-            Log.v(TAG, "Keypress detected");
-            MQTTUtils.pub("ECS/state", "5");
-        }
-        return super.onKeyDown(keyCode, event);
+
+        //Send your own custom broadcast message
+        Log.v(TAG, "Keypress detected");
+        myMqtt.pub("MEDIA/ATV/KeyPress", String.valueOf(keyCode));
+
+       // switch(keyCode)
+
+
+       // return super.onKeyDown(keyCode, event);
+        return true;
     }
 
     public void gerard (){
